8fa278daae8bbbe794f7d4c2730a90d35c34584a
baenaale <alejandrabaena32@gmail.com>
2021-10-10 10:49:05 -0500

    created a weatherforecastday component and displayed temp for the next 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-    setForecast(response.data.daily);
+    setForecast(response.data);
     setLoaded(true);
   }
 
@@ -17,14 +17,16 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <div className="WeatherForecast-day">Sun</div>
-            <WeatherIcon code="01d" size={30} />
-            <div className="WeatherForecast-temperatures">
-              <span className="WeatherForecast-temperature-max">°</span>
-              <span className="WeatherForecast-temperature-min">74°</span>
-            </div>
-          </div>
+          {forecast.list.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       </div>
     );
@@ -32,8 +34,8 @@ export default function WeatherForecast(props) {
     let apiKey = "4f368074f0c183534aaa3f5cab5a3038";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-    return null;
+    return "Loading...";
   }
 }
